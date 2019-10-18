6c5d96e508e74fe4ab8d878776243cecd4423e5c
Pablo Zandoná <P!a@4679>
2019-10-17 23:49:50 -0300

    deploy

--- www/vue.config.js
+++ www/vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
     ? '/medical-appointment-classifier/'
-    : '/',
+    : '',
     outputDir: path.resolve(__dirname, "../docs"),
-    assetsDir: "../docs"
+    // assetsDir: "../docs"
 };
